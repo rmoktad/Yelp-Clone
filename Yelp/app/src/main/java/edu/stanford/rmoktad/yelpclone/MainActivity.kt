43cfbe7867453c707ac3b4180b58c90580417de6
rmokt <61708981+rmokt@users.noreply.github.com>
2020-05-13 19:02:49 -0600

    Added check for network connection when first opening the app

--- Yelp/app/src/main/java/edu/stanford/rmoktad/yelpclone/MainActivity.kt
+++ Yelp/app/src/main/java/edu/stanford/rmoktad/yelpclone/MainActivity.kt
@@ -1,9 +1,13 @@
 package edu.stanford.rmoktad.yelpclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.DialogInterface
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.util.Log
-import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import edu.stanford.rmoktad.myapplication.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,11 +16,40 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+
 
 private const val  BASE_URL = "https://api.yelp.com/v3/"
 private const val TAG = "MainActivity"
 private const val API_KEY = "j_yq9VhoxQuL-CH8CC8-AWXDN2O-V6N7tbSNPi__gHmo2raYEquA7TfjpBcLUtCyy67ibsOK6fzT-g3PizTrUQK9n11jnMtZvlQ--4FnPnCZSQEFbQ3EoYIP-UW8XnYx"
 class MainActivity : AppCompatActivity() {
+
+
+//    private fun isNetworkAvailable():Boolean {
+//        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        val activeNetworkInfo: NetworkInfo = connectivityManager?.activeNetworkInfo
+//        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting()
+//    }
+
+    private fun isNetworkAvailable(): Boolean? {
+        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
+
+    fun isOnline(): Boolean {
+        val runtime = Runtime.getRuntime()
+        try {
+            val ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8")
+            val exitValue = ipProcess.waitFor()
+            return exitValue == 0
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        return false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +62,27 @@ class MainActivity : AppCompatActivity() {
 
         val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
         val yelpService = retrofit.create(YelpService::class.java)
+
+        if(isNetworkAvailable()  == false/*|| !isOnline()*/){
+                // build alert dialog
+                val dialogBuilder = AlertDialog.Builder(this)
+
+                // set message of alert dialog
+                dialogBuilder.setMessage("Please connect to a network!")
+                    // if the dialog is cancelable
+                    .setCancelable(false)
+                    // positive button text and action
+                    .setPositiveButton("OK", DialogInterface.OnClickListener {
+                            dialog, id -> dialog.cancel()
+                    })
+
+                // create dialog box
+                val alert = dialogBuilder.create()
+                // set title for alert dialog box
+                alert.setTitle("No Network Connection")
+                // show alert dialog
+                alert.show()
+        }
 
         //operation is asynchronous
         yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object: Callback<YelpSearchResult>{
@@ -50,4 +104,5 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
 }
